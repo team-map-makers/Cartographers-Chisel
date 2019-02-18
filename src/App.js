398ef79a82bafb8a45b7698cb761d49e84364097
nicster34 <Mncdk3435!>
2019-02-17 20:09:02 -0500

    Configured chrome debugger tools

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("help")
+    var themtree = 89;
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+      
+    }));
+    console.log(themtree);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" onClick={this.handleClick}/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button onClick={this.handleClick} >Hi</button>
           <a
             className="App-link"
             href="https://reactjs.org"

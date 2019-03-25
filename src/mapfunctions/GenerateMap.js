1d4d6a6c4ced2f7d62d34ec0b17a1a952fce2776
Tarek <td_1995@live.com>
2019-03-25 12:42:25 -0400

    hwo

--- src/mapfunctions/GenerateMap.js
+++ src/mapfunctions/GenerateMap.js
@@ -1,29 +1,32 @@
 import * as d3 from 'd3';
 class GenerateMap {
 
-  
+
   constructor() {
     this.init = this.init.bind(this);
   }
-  mapData ={};
-  init(){
+  mapData = {};
+  init() {
     var MAP_HEIGHT = 1000;
     var MAP_WIDTH = 2000;
     //his.mapData.width =2000;
     //this.mapData.height =1000;
-    this.mapData.sites = d3.range(1000).map(function(d) {
-      return [Math.random(1) * MAP_WIDTH, Math.random(1) * MAP_HEIGHT];
+    this.mapData.sites = d3.range(1000).map(function (d) {
+      return [Math.random(2) * MAP_WIDTH, Math.random(2) * MAP_HEIGHT];
     });
-    var voronoi = d3.voronoi().extent([[0, 0],[MAP_WIDTH, MAP_HEIGHT]]);
+    var voronoi = d3.voronoi().extent([
+      [0, 0],
+      [MAP_WIDTH, MAP_HEIGHT]
+    ]);
     this.mapData.diagram = voronoi(this.mapData.sites);
     var color = d3.scaleSequential(d3.interpolateSpectral);
     this.mapData.cells = [];
     //debugger;
-    this.mapData.cells = this.mapData.diagram.polygons().map(function(path, id) {
-      var cell ={};
+    this.mapData.cells = this.mapData.diagram.polygons().map(function (path, id) {
+      var cell = {};
       cell.key = id;
       cell.d = "M" + path.join("L") + "Z";
-      cell.fill=color(id/2000);
+      cell.fill = color(id / 2000);
       return cell;
     })
     return this.mapData.cells
@@ -38,7 +41,7 @@ class GenerateMap {
   //     diagram = voronoi(sites), polygons = diagram.polygons(),
   //       // Add spectral color range[0,1] using d3-scale-chromatic
   //     color = d3.scaleSequential(d3.interpolateSpectral); 
-       
+
   //     // Draw the colored polygons
   //   polygons.map(function(i, d) {
   //     svg.append("path")
@@ -49,9 +52,8 @@ class GenerateMap {
   //       //relax(svg,sites,diagram,polygons,voronoi,color);
   //       //addIsland();
   // }
-       
+
   //     // Adding relax function
 }
 
 export default GenerateMap;
-
